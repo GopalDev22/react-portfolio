cdb8cec75010b2d92b6ad2e866d0dd903f57b877
venu gopal <iamdeveloper.react@gmail.com>
2024-12-27 07:19:30 +0530

    fixed animation issues

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -20,78 +20,43 @@ export const Technology = () => {
       </motion.h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
         <motion.div
-          initial={{ y: -10 }}
-          animate={{
-            y: [10, -10],
-            transition: {
-              duration: 1.5,
-              ease: "linear",
-              repeat: Infinity,
-              repeatType: "reverse",
-            },
-          }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 1, delay: 0 }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <RiReactjsLine className="text-7xl text-cyan-400" />
         </motion.div>
         <motion.div
-          initial={{ y: -10 }}
-          animate={{
-            y: [10, -10],
-            transition: {
-              duration: 2,
-              ease: "linear",
-              repeat: Infinity,
-              repeatType: "reverse",
-            },
-          }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 1.5, delay: 0 }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <SiRedux className="text-7xl text-purple-600" />
         </motion.div>
         <motion.div
-          initial={{ y: -10 }}
-          animate={{
-            y: [10, -10],
-            transition: {
-              duration: 2.5,
-              ease: "linear",
-              repeat: Infinity,
-              repeatType: "reverse",
-            },
-          }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 2, delay: 0 }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <RiJavascriptLine className="text-7xl text-yellow-600" />
         </motion.div>
 
         <motion.div
-          initial={{ y: -10 }}
-          animate={{
-            y: [10, -10],
-            transition: {
-              duration: 1.75,
-              ease: "linear",
-              repeat: Infinity,
-              repeatType: "reverse",
-            },
-          }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 2.5, delay: 0 }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <RiTailwindCssLine className="text-7xl text-cyan-400" />
         </motion.div>
 
         <motion.div
-          initial={{ y: -10 }}
-          animate={{
-            y: [10, -10],
-            transition: {
-              duration: 1,
-              ease: "linear",
-              repeat: Infinity,
-              repeatType: "reverse",
-            },
-          }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 3, delay: 0 }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <BiLogoTypescript className="text-7xl text-cyan-600" />
